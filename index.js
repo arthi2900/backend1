d4ea1f66e4a8b9b35ac5590472115b4285d8ba61
arthi2900 <aaarthi2927@gmail.com>
2022-06-26 00:03:44 -0700

    add

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ console.log("hi connect");
 import  express from 'express';
 import  cors from 'cors';
 import {MongoClient} from "mongodb";
-const PORT=4000 ||3001 ;
-const MONGO_URL='mongodb+srv://arthi:arthi@cluster0.ayv6i.mongodb.net';
+const PORT=process.env.PORT ||3001 ;
+const MONGO_URL=process.env.MONGO_URL;
 
 export const app=express(); 
 app.use(express.json());
@@ -13,13 +13,15 @@ app.use(cors());
         res.json({ message: "welcome ARTHI" });
       });
             */
-      
-      async function createConnection(){
+            async function createConnection(){
         const client=new MongoClient(MONGO_URL);
         await client.connect();
         console.log("mongo connect");
         return client;
     }
+    app.get("/User", (req, res) => {
+      res.json({ message: "welcome ARTHI" });
+    });
     export const client=await createConnection();
       app.listen(PORT, () => {
         console.log(`Server listening on ${PORT}`);
