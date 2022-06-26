1d3c39241dc79045d5d73ebc68d9d799897b75c3
arthi2900 <aaarthi2927@gmail.com>
2022-06-25 23:55:37 -0700

    add

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 console.log("hi connect");
-const express=require('express')
-const cors= require('cors')
-const app=express()
-const PORT= 4000 ||3001 ;
+import  express from 'express';
+import  cors from 'cors';
+import {MongoClient} from "mongodb";
+const app=express();
+const PORT=4000 ||3001 ;
+const MONGO_URL='https://62642a15a6adc673188e65dd.mockapi.io/User';
+app.use(express.json());
 app.use(cors());
-app.get("/api", (req, res) => {
-        res.json({ message: "welcome all" });
+
+    /*app.get("/api", (req, res) => {
+        res.json({ message: "welcome ARTHI" });
       });
+            */
       
+      async function createConnection(){
+        const client=new MongoClient(MONGO_URL);
+        await client.connect();
+        console.log("mongo connect");
+        return client;
+    }
+    export const client=await createConnection();
       app.listen(PORT, () => {
         console.log(`Server listening on ${PORT}`);
       });
